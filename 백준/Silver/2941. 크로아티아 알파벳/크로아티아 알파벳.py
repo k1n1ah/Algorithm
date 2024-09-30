77dc3fbb44767765f95ba5bba043a937a195fbf6
k1n1ah <101460952+k1n1ah@users.noreply.github.com>
2024-10-01 07:22:24 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,9 +1,9 @@
 S = input()
-A = ["c=","c-","dz=","d-","lj","nj","s=","z="]
+A = ["c=", "c-","dz=","d-","lj","nj","s=","z="]
 count = 0
-
-for word in A:
-    count += S.count(word)  # 패턴이 몇 번 나오는지 센다
-    S = S.replace(word, " ")  # 찾은 패턴을 제거
-
-print(count + len(S.replace(" ", "")))  # 남은 문자도 추가
+for word in A :
+        count += S.count(word)
+        S = S.replace(word,' ') #남은 알파벳들이 모여서 A에 있는 문자를 만들수도 있으므로 
+print(count + len(S.replace(' ', '')))
+        
+    
